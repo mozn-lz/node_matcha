2d89091309ae006c5c0d82001f805311ac82de8d
mozn-lz <mozn.lozn2000@gmail.com>
2019-02-23 03:04:14 +0200

    adding validation

--- routes/register.js
+++ routes/register.js
@@ -14,13 +14,86 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	var user	= req.body.username;
-	var email	= req.body.email;
-	var name	= req.body.name;
-	var surname	= req.body.surname;
-	var psswd	= req.body.psswd;
-	var psswd1	= req.body.psswd1;
+	if (!is_empty(req.body.username)){
+		var user	= req.body.username;
+	} else {
+		error_log[i++] = ' is invalid';
+	}
 
+	if (!is_empty(req.body.email)){
+		var email	= req.body.email;
+	} else {
+		error_log[i++] = ' is invalid';
+	}
+
+	if (!is_empty(req.body.name)){
+		var name	= req.body.name;
+	} else {
+		error_log[i++] = ' is invalid';
+	}
+
+	if (!is_empty(req.body.surname)){
+		var surname	= req.body.surname;
+	} else {
+		error_log[i++] = ' is invalid';
+	}
+
+	if (!is_empty(req.body.psswd)){
+		var psswd	= req.body.psswd;
+	} else {
+		error_log[i++] = ' is invalid';
+	}
+
+	if (!is_empty(req.body.psswd1)){
+		var psswd1	= req.body.psswd1;
+	} else {
+		error_log[i++] = ' is invalid';
+	}
+
+	/* checks if there are any errors in saving variables from the user
+	 and if passwords match	*/
+	if (i == -1 && is_match(psswd, psswd1)) {
+		// store data to JSON array, to store in mongo
+		var usr_data = {
+			usr_user : user,
+			usr_email : email,
+			usr_name : name,
+			usr_surname : surname,
+			usr_psswd : psswd,		// to be encrypted
+			login_time : '',
+			pic : [],
+			gender : '',
+			oriantation : '',
+			rating : '',
+			bio : '',
+			intrests : [],
+			gps : '',
+			viewd : [],
+			liked : [],
+			verified : 0,
+			confirm_code : Math.random()	// to be encrypted
+		};
+	
+		// Connect and save data to mongodb
+		MongoClient.connect(url, function(err, client) {
+			assert.equal(null, err);
+			console.log("Connected to server and mongo connected Successfully");
+			const db = client.db(dbName);
+	
+			const collection = db.collection('users');
+	
+			collection.insertOne(usr_data, function(err, result) {
+				assert.equal(null, err);
+				console.log("Documents added to database: " + dbName);
+				client.close();
+				 res.redirect('/login/' + user);
+			});
+		});
+	} else {
+		// if passwords don't match or there are errors in the code
+		error_log[i++] = 'Password error';
+	}
+	
 	console.log('username: '	+ user + "\n");
 	console.log('email: '	+ email + "\n");
 	console.log('name: '	+ name + "\n");
@@ -28,39 +101,6 @@ router.post('/', function(req, res, next) {
 	console.log('psswd: '	+ psswd + "\n");
 	console.log('psswd1: '	+ psswd1 + "\n");
 
-	var usr_data = {
-		usr_id : '',
-		usr_user : user,
-		usr_email : email,
-		usr_name : name,
-		usr_surname : surname,
-		usr_psswd : psswd,
-		login_time : '',
-		pic : [],
-		gender : '',
-		oriantation : '',
-		rating : '',
-		bio : '',
-		intrests : [],
-		gps : '',
-		viewd : [],
-		liked : []
-	};
-
-	MongoClient.connect(url, function(err, client) {
-		assert.equal(null, err);
-		console.log("Connected to server and mongo connected Successfully");
-		const db = client.db(dbName);
-
-		const collection = db.collection('users');
-
-		collection.insertOne(usr_data, function(err, result) {
-			assert.equal(null, err);
-			console.log("Documents added to database: " + dbName);
-			client.close();
-			 res.redirect('/login/' + user);
-		});
-	});
 	// mongo.connect(url, function(err, db) {
 	// 	assert.equal(null, err);
 	// 	db.collection('users').insertOne(usr_data, function(err, result) {
