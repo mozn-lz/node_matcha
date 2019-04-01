f7e12875dfe265e63f7b9ed5a191d9d611dfc557
mozn-lz <mozn.lozn2000@gmail.com>
2019-04-01 20:47:52 +0200

    added testing code

--- routes/helper_functions.js
+++ routes/helper_functions.js
@@ -2,9 +2,12 @@ module.exports = {
 	// Finds ireq.body.psswds string is empty
 	is_empty: function (str) {
 		ret = str.trim();
+		
 		if (ret.length == 0) {
+			console.log('\t\tis_empty: Returning true for ' + str + ' of length ' + ret.length);
 			return (true);
 		}
+		console.log('\t\tis_empty: Returning false for ' + str + ' of length ' + ret.length);
 		return (false);
 	},
 
