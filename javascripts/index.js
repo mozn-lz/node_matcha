1fc2c0ab2d0844203f4a391bb3cac9f0fb41e96a
mozn-lz <mozn.lozn2000@gmail.com>
2019-10-18 15:12:37 +0200

    send message button on index works

---
+++ javascripts/index.js
@@ -0,0 +1,8 @@
+
+// window.location.assign("new target URL");
+
+goViewProfile = (user) => {
+	// window.location.href = myLoginUrl;
+	// console.log(user)
+	// window.location.href = "/views_profile/" + user;
+}
--- routes/view_messages.js
+++ routes/view_messages.js
@@ -1,10 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';	// Database(mongo) url
+const dbName = 'matcha';		// Database Name
+const page_name = 'View Messages';		// page name
 
 /* GET view_messages listing. */
-router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-  res.render('view_messages', { page: 'View Messages' });
+router.get('/', function (req, res, next) {
+	//   res.send('respond with a resource');
+	res.render('view_messages', { page: 'View Messages' });
+});
+
+router.get('/:user', function (req, res, next) {
+	let chatId = req.params.user;
+	
+	MongoClient.connect(url, function (err, client) {
+		assert.equal(null, err);
+		const db = client.db(dbName);
+		var find_user = [];
+		const collection = db.collection('users');
+
+		collection.find({'_id': objectId(chatId)}).forEach(function (doc, err) {
+			assert.equal(null, err);
+			find_user.push(doc);
+			console.log('\t\t doc: ' + doc);
+			
+		}, function () {
+			client.close();
+			console.log('find_user.length: ', find_user.length);
+			if (find_user.length == 1) {
+				console.log('if: find_user.length', find_user.length == 1);
+				 res.render('view_messages', {
+					page: 'View Messages',
+					id: chatId,
+					chat_data : find_user[0]
+				});
+			} else {
+				console.log("Else " + chatId + " not found");
+				// res.redirect('/messages');
+				//  (chatId.search('pass_suc') == 0) ? res.render(page_name, {
+			}
+			console.log('exiting render misfunction');			
+		});
+	});
 });
 
 module.exports = router;
