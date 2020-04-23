04eab9395013b907125c0c71bea693b129bddce9
mozn-lz <mozn.lozn2000@gmail.com>
2020-04-23 08:00:00 +0200

    fixed broken view porile

--- routes/view_profile.js
+++ routes/view_profile.js
@@ -37,8 +37,8 @@ router.get('/:reqId', (req, res, next) => {
 	console.log("************View Profle************\n");
 	
 	if (req.session.uid) {
-		let friendReqId = req.params.reqId
-		console.log("friendId: ", friendReqId);
+		let friendId = req.params.reqId
+		console.log("friendId: ", friendId);
 
 		let notification = {	// notification object
 			from : req.session.uid,
@@ -50,47 +50,36 @@ router.get('/:reqId', (req, res, next) => {
 			const db = client.db(dbName);
 			var find_user = [];
 			const collection = db.collection('users');
-
-			collection.find({ '_id': objectId(friendReqId) }).forEach((doc, err) => {
+			// db.collection('users').find({ '_id': objectId(friendId) }).forEach((docs, err) => {
+			db.collection('users').find({ '_id': objectId(friendId) }).forEach((doc, err) => {
 				assert.equal(null, err);
 				// (!doc.profile) ? doc.profile = "/images/ionicons.designerpack/md-person.svg" : 0;
 				find_user.push(doc);
 				console.log('\t\t doc: ' + doc);
-
-			},( () => {
-
-				collection.updateOne({ '_id': objectId(friendReqId) }, 	// send norification to 'friend'
+			}
+			,() => {
+				collection.updateOne({ '_id': objectId(friendId) }, 	// send norification to 'friend'
 				{$addToSet: {
 					notifications : notification
 				}});
 				console.log('\t\t Sending notification');
-			})(), (() => {
-
+			// }
+			// , () => {
 				console.log('\t\t Closing databse connection');
 				client.close();
 				console.log('find_user.length: ', find_user.length);
 				let message = '';
 				if (find_user.length == 1) {
-					/*******************************/
-					/*     make friend request     */
-					/*******************************/
-					console.log('if: find_user.length', find_user.length == 1);
-					message = "pass_sucFriend request has been made";
-					console.log("\n\t\tmessage ", message, "\n");
-					(!find_user[0].profile) ? find_user[0].profile = "/images/ionicons.designerpack/md-person.svg" : 0;
-					(find_user[0] == 'show') ? find_user[0].gps = gps : find_user[0].gps = '';		//
 					renderProfile(res, find_user[0]);
 				} else {
-					console.log("Else " + friendReqId + " not found");
-					// res.redirect('/messages');
-					//  (friendReqId.search('pass_suc') == 0) ? res.render(page_name, {
+					console.log("Else " + friendId + " not found");
 					message = "pass_errError: friend requiest unsuccessfill, please try again";
 					console.log("\n\t\tmessage ", message, "\n");
-					res.redirect('/index')
-					// renderProfile(res, find_user);
+					res.redirect('/index');
 				}
 				console.log('exiting render misfunction');
-			})());
+			}
+			);
 		});
 
 	} else {
