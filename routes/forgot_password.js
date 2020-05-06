79665e44515c9e523b0cce9d576cce37bdbc2156
mozn-lz <mozn.lozn2000@gmail.com>
2020-05-06 05:15:18 +0200

    init forgot_password

--- routes/forgot_password.js
+++ routes/forgot_password.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET forgot_password listing. */
-router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-  res.render('forgot_password', { page: 'Forgot Password' });
+router.get('/', function (req, res, next) {
+	//   res.send('respond with a resource');
+	res.render('forgot_password', { page: 'Forgot Password' });
+});
+
+router.post('/', function (req, res, next) {
+	//   res.send('respond with a resource');
+	MongoClient.connect(url, (error, client) => {
+		db.client.collection('users').find({usr_email: res.body.email}).forEach((err, doc) => {
+			user.push(doc);
+		}, () => {
+			user[0].usr_email;	// to email
+			user[0].usr_user;	//	to name
+			user[0].usr_code = Math.random();	//	verification email
+		});
+	});
+
+	res.render('forgot_password', { page: 'Forgot Password' });
 });
 
 module.exports = router;
