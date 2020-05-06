31650463870563b9a07007c70aa7e9d6c7e4e9a4
mozn-lz <mozn.lozn2000@gmail.com>
2020-05-06 04:33:10 +0200

    addad 'visit history'

--- routes/view_profile.js
+++ routes/view_profile.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const objectId = require('mongodb').ObjectID;
+let objectId = require('mongodb').ObjectID;
 const assert = require('assert');
 var helper = require('./helper_functions'); // Helper functions Mk
 var helper_index = require('./helper_index'); // Helper functions Mk
@@ -12,37 +12,39 @@ const dbName = 'mk_matcha';					// Database Name
 var page_name = 'view_profile';
 
 let renderProfile = (res, data) => {
-	console.log('rendering page: ', page_name);
+	// console.log('profile pic ', data.profile);
+	console.log('data.history ', data.history);
+	// console.log('rendering page: ', page_name);
 	res.render(page_name, {
-		title	: page_name,
-		id		: data._id,
-		picture	: data.pic,
+		title: page_name,
+		id: data._id,
+		picture: data.profile,
 		username: data.usr_user,
-		fname	: data.usr_name,
-		lname	: data.usr_surname,
-		bio			: data.bio,
-		intrests	: data.intrests,
-		pic		: data.pic,
-		age		: data.age,
-		gender	: data.gender,
-		rating	: data.rating,
-		gps		: data.gps,
-		viewd	: data.viewd,
-		liked	: data.liked
+		fname: data.usr_name,
+		lname: data.usr_surname,
+		bio: data.bio,
+		intrests: data.intrests,
+		pic: data.pic,
+		age: data.age,
+		gender: data.gender,
+		rating: data.rating,
+		gps: data.gps,
+		viewd: data.viewd,
+		liked: data.liked,
+		history: data.history
 	});
 }
 
-
 router.get('/:reqId', (req, res, next) => {
 	console.log("************View Profle************\n");
-	
+
 	if (req.session.uid) {
 		let friendId = req.params.reqId
-		console.log("friendId: ", friendId);
 
+		console.log('friendid -_-: ', friendId);
 		let notification = {	// notification object
-			from : req.session.uid,
-			type : 'view profile'
+			from: req.session.uid,
+			type: 'view profile'
 		}
 
 		MongoClient.connect(url, (err, client) => {
@@ -50,38 +52,41 @@ router.get('/:reqId', (req, res, next) => {
 			const db = client.db(dbName);
 			var find_user = [];
 			const collection = db.collection('users');
-			// db.collection('users').find({ '_id': objectId(friendId) }).forEach((docs, err) => {
-			db.collection('users').find({ '_id': objectId(friendId) }).forEach((doc, err) => {
+			collection.find({ '_id': objectId(friendId) }).forEach((doc, err) => {
 				assert.equal(null, err);
-				// (!doc.profile) ? doc.profile = "/images/ionicons.designerpack/md-person.svg" : 0;
 				find_user.push(doc);
 				console.log('\t\t doc: ' + doc);
-			}
-			,() => {
+			}, () => {
+				collection.updateOne({ '_id': objectId(req.session.uid) }, 	// Add to visit history
+					{
+						$addToSet: {
+							history: {
+								id: find_user[0].uid,
+								name: find_user[0].name,
+								surname: find_user[0].surname,
+								date: Date.now()
+							}
+						}
+					});
 				collection.updateOne({ '_id': objectId(friendId) }, 	// send norification to 'friend'
-				{$addToSet: {
-					notifications : notification
-				}});
-				console.log('\t\t Sending notification');
-			// }
-			// , () => {
-				console.log('\t\t Closing databse connection');
+					{
+						$addToSet: {
+							notifications: notification
+						}
+					});
 				client.close();
-				console.log('find_user.length: ', find_user.length);
 				let message = '';
 				if (find_user.length == 1) {
-					renderProfile(res, find_user[0]);
+					helper.findUserById(req.session.uid, (user) => {
+						find_user[0].history = user.history;	// bad code #quickfix
+						renderProfile(res, find_user[0]);
+					});
 				} else {
-					console.log("Else " + friendId + " not found");
 					message = "pass_errError: friend requiest unsuccessfill, please try again";
-					console.log("\n\t\tmessage ", message, "\n");
 					res.redirect('/index');
 				}
-				console.log('exiting render misfunction');
-			}
-			);
+			});
 		});
-
 	} else {
 		res.redirect('/login/' + 'pass_errYou have to be logged in to view the ' + page_name + ' page ');
 	}
