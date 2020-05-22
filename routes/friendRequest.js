18de58dd722fcea0a1badae518cc1c7e2e9eb661
mozn-lz <mozn.lozn2000@gmail.com>
2020-05-22 19:38:26 +0200

    friend request, blocking and fake accounts

--- routes/friendRequest.js
+++ routes/friendRequest.js
@@ -10,7 +10,7 @@ const url = 'mongodb://localhost:27017';	// Database Address
 const dbName = 'mk_matcha';;					// Database Name
 var page_name = 'friend request';
 
-(req.session.uid) ? helper.logTme : 0;	//	update last online
+// (req.session.uid) ? helper.logTme : 0;	//	update last online
 
 /* GET view_profile listing. */
 router.get('/', function (req, res, next) {
@@ -19,6 +19,81 @@ router.get('/', function (req, res, next) {
 	res.redirect('/index');
 });
 
+router.post('/', (req, res, next) => {
+
+	console.log('\n\t\tPOST friend request POST');
+
+	let message = null;
+	if (req.session.uid) {
+
+		let submit = req.body.submit;
+		let friendReqId = req.body.friendId;
+
+		// Connect and save data to mongodb
+		MongoClient.connect(url, function (err, client) {
+			assert.equal(null, err);
+			console.log("\tConnected to server and mongo connected Successfully");
+			const db = client.db(dbName);
+			const collection = db.collection('users');
+
+			let notification = {	// notification object
+				from: req.session.uid,
+				type: 'friend request'
+			}
+
+			console.log('fridend: ', friendReqId, '\nsubmit: ', submit);
+			// client.db(dbName).collection('users').find({})
+			if (submit === 'add') {
+				console.log('adding...' + friendReqId, '\n');
+				res.redirect('/friendRequest/' + friendReqId);
+			} else if (submit === 'block') {
+				notification.type = 'block';
+				collection.updateOne({
+					'_id': objectId(req.session.uid)
+				}, {
+					$addToSet: {
+						blocked: friendReqId
+					}
+				}, (() => {
+					console.log('Sending notification');
+					// collection.updateOne({ '_id': objectId(friendReqId) }, { 	// send norification to 'friend'
+					// 	$addToSet: {
+					// 		notifications: notification
+					// 	}
+					// });
+				})(), (err, result) => {
+					console.log('Hahahah, notifications are fucking up? <<< We are Bolckign in this mother >>>');
+					client.close();
+					message = 'pass_sucUser has been blacked';
+					res.redirect('/index/' + message);
+				});
+			} else if (submit === 'fake') {
+				console.log('fake account');
+				collection.updateOne({ '_id': objectId(friendReqId) }, {
+					$set: {
+						verified: 2
+					}
+				}, (() => {
+					console.log('Sending notification to ', friendReqId, 'from');
+					collection.updateOne({ '_id': objectId(friendReqId) }, { 	// send norification to 'friend'
+						$addToSet: {
+							notifications: notification
+						}
+					});
+				})(), (err, result) => {
+					console.log('<<< shit is fake in this mother >>>');
+					console.log('Hahahah, notifications are fucking up');
+					client.close();
+					message = 'pass_sucFake user reported';
+					res.redirect('/index/' + message);
+				});
+			} else {
+				res.redirect('/index');
+			}
+			// helper.findUserById(req.body.friendId, ())
+		});
+	}
+});
 
 router.get('/:reqId', (req, res, next) => {
 	console.log("************Friend req(ARG)************\n");
@@ -27,7 +102,7 @@ router.get('/:reqId', (req, res, next) => {
 	if (req.session.uid) {
 		helper.findUserById(req.session.uid, (user) => {
 			// if (user.picture)
-			if (user.picture && user.picture.length <= 1) {
+			if (user.picture && user.picture.length >= 1) {
 				let friendReqId = req.params.reqId
 				console.log("1. usrId: ", req.session.uid, '\n');
 				console.log("1. friendId: ", friendReqId, '\n');
@@ -36,12 +111,16 @@ router.get('/:reqId', (req, res, next) => {
 					from: req.session.uid,
 					type: 'friend request'
 				}
+				console.log('friendReqId ', friendReqId);
 				// Connect and save data to mongodb
 				MongoClient.connect(url, function (err, client) {
 					assert.equal(null, err);
 					console.log("\tConnected to server and mongo connected Successfully");
 					const db = client.db(dbName);
 					const collection = db.collection('users');
+
+					console.log('friendReqId ', friendReqId);
+
 					collection.updateOne({
 						'_id': objectId(friendReqId)
 					}, {
@@ -50,12 +129,11 @@ router.get('/:reqId', (req, res, next) => {
 						}
 					}, (() => {
 						console.log('Sending notification');
-						collection.updateOne({ '_id': objectId(friendReqId) }, 	// send norification to 'friend'
-							{
-								$addToSet: {
-									notifications: notification
-								}
-							});
+						collection.updateOne({ '_id': objectId(friendReqId) }, {	// send norification to 'friend'
+							$addToSet: {
+								notifications: notification
+							}
+						});
 					})(), (err, result) => {
 						console.log('Hahahah, notifications are fucking up');
 						client.close();
@@ -64,6 +142,8 @@ router.get('/:reqId', (req, res, next) => {
 					});
 				});
 			} else {
+				console.log(user.picture);
+
 				message = 'pass_errPlease upouad a picture first';
 				res.redirect('/index/' + message);
 			}
