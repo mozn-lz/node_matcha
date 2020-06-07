3deea91bfd396a9f8f10e7e55f91a12355c1bf41
mozn-lz <mozn.lozn2000@gmail.com>
2020-06-07 15:51:28 +0200

    used helper_functions to shorten code

--- routes/notify.js
+++ routes/notify.js
@@ -4,10 +4,11 @@ const MongoClient = require('mongodb').MongoClient;
 const objectId = require('mongodb').ObjectID;
 const assert = require('assert');
 
+const helper = require('./helper_functions'); // Helper functions Mk
+const message_helper = require('./Helper_messages');
+
 const url = 'mongodb://localhost:27017';	// Database Address
 const dbName = 'mk_matcha';;					// Database Name
-
-// (req.session.uid) ? helper.logTme : 0;	//	update last online
 
 router.get('/', function (req, res) {
 	let page_name = 'notify';
@@ -18,23 +19,22 @@ router.get('/', function (req, res) {
 		MongoClient.connect(url, (err, client) => {
 			const collection = client.db(dbName).collection('users');
 			assert.equal(err, null);
-			collection.find({ '_id': objectId(req.session.uid) }).forEach((doc, err) => {
-				assert.equal(err, null);
-				usersArray.push(doc);
-			}, () => {
-				if (usersArray) {
-					(usersArray[0].notifications) ? notifications = usersArray[0].notifications : notifications = null;
-					let time = Date.now();
-					collection.updateOne({ '_id': objectId(usersArray[0]._id) }, {
-						$set: { login_time: time }
-					});
-					// console.log(notifications);
-					setTimeout(() => {
-						client.close();
-						res.send({ notifications });
-					}, 1000);
-				}
-			});
+			let time = Date.now();
+			collection.updateOne({ '_id': objectId(usersArray._id) }, {
+				$set: { login_time: time }
+			}, () => client.close());
+		});
+
+		helper.findUserById(req.session.uid, (usersArray) => {
+			if (usersArray) {
+				console.log('user found');
+				(usersArray.notifications) ? notifications = usersArray.notifications : notifications = null;
+				setTimeout(() => {
+					res.send({ notifications });
+				}, 1000);
+			} else {
+				console.log('user not found');
+			}
 		});
 	}
 	//  else {
@@ -49,7 +49,7 @@ router.post('/', (req, res) => {
 		location.country = data.country;
 		location.city = data.city;
 		location.timezone = data.timezone;
-		console.log('\t\t Location* ',location);
+		console.log('\t\t Location* ', location);
 		req.session.gps = location;
 		MongoClient.connect(url, (err, client) => {
 			assert.equal(null, err);
